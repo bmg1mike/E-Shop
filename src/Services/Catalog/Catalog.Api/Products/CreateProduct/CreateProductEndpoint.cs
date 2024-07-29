15af8213668e0f98df13e2040b390629db2d7406
Michael Boniface <bmg1mike@gmail.com>
2024-07-29 10:06:19 +0100

    chore: Add E-ShopMicroservice solution file

---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using Carter;
+using MediatR;
+
+namespace Catalog.Api;
+
+public record CreateProductRequest(string Name, List<string> Categories, string Description, string ImageFile, decimal Price);
+public record CreateProductResponse(Guid Id);
+public class CreateProductEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/products", async (ISender _mediator, CreateProductRequest request) =>
+        {
+            var command = MapToCommand(request);
+            var result = await _mediator.Send(command);
+            return new CreateProductResponse(result.Id);
+
+        });
+    }
+
+    private CreateProductCommand MapToCommand(CreateProductRequest request)
+    {
+        return new CreateProductCommand(request.Name, request.Categories, request.Description, request.ImageFile, request.Price);
+    }
+}
---
+++ src/Services/Catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks;
+
+namespace Catalog.Api;
+
+public record CreateProductCommand(string Name, List<string> Categories, string Description, string ImageFile, decimal Price) : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    // private readonly IProductRepository _productRepository;
+
+    // public CreateProductCommandHandler(IProductRepository productRepository)
+    // {
+    //     _productRepository = productRepository;
+    // }
+
+    public async Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = request.Name,
+            Categories = request.Categories,
+            Description = request.Description,
+            ImageFile = request.ImageFile,
+            Price = request.Price
+        };
+
+        // await _productRepository.AddAsync(product);
+
+        return new CreateProductResult(product.Id);
+    }
+}
+
