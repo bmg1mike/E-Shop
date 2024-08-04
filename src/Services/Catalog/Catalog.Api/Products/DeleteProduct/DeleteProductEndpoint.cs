1d8760c11c9de76b58f56eebf18c70b58b92ffbf
Michael Boniface <bmg1mike@gmail.com>
2024-08-04 05:04:11 +0100

    chore: Update DeleteProductEndpoint and UpdateProductEndpoint

--- src/Services/Catalog/Catalog.Api/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/Catalog.Api/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -11,7 +11,13 @@ public class DeleteProductEndpoint : ICarterModule
             var command = MapToCommand(id);
             var result = await sender.Send(command);
             return Results.Ok(new DeleteProductResponse(result.IsSuccess));
-        });
+        }).WithName("DeleteProduct")
+        .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithSummary("Delete a product")
+        .WithDescription("Delete a product");
     }
 
     private DeleteProductCommand MapToCommand(Guid id) => new DeleteProductCommand(id);
--- src/Services/Catalog/Catalog.Api/Products/UpdateProduct/UpdateProductEndpoint.cs
+++ src/Services/Catalog/Catalog.Api/Products/UpdateProduct/UpdateProductEndpoint.cs
@@ -12,7 +12,14 @@ public class UpdateProductEndpoint : ICarterModule
             var command = MapToCommand(request);
             var result = await sender.Send(command);
             return Results.Ok(new UpdateProductResponse(result.isSuccess));
-        });
+        })
+        .WithName("UpdateProduct")
+        .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .ProducesProblem(StatusCodes.Status404NotFound)
+        .ProducesProblem(StatusCodes.Status500InternalServerError)
+        .WithSummary("Update a product")
+        .WithDescription("Update a product");
     }
 
     private UpdateProductCommand MapToCommand(UpdateProductRequest request) => new(request.Product);
